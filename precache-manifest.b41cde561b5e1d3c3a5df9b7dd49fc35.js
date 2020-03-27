95e878d5a8a4164271dc19a776a4a684aed784db
Meliodas <romamur65@gmail.com>
2020-03-27 02:51:56 +0200

    Updates

---
+++ precache-manifest.b41cde561b5e1d3c3a5df9b7dd49fc35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd428cc4d2139b64e6c893035c41e9cf",
+    "url": "/untitled8/index.html"
+  },
+  {
+    "revision": "20d6046501fb7b026c30",
+    "url": "/untitled8/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81211e74dad95cbef158",
+    "url": "/untitled8/static/js/2.d7214e0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/untitled8/static/js/2.d7214e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d6046501fb7b026c30",
+    "url": "/untitled8/static/js/main.25104d6b.chunk.js"
+  },
+  {
+    "revision": "448d3c79c8cf540acca3",
+    "url": "/untitled8/static/js/runtime-main.66b34098.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/untitled8/static/media/logo.5d5d9eef.svg"
+  }
+]);
